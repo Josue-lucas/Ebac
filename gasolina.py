2a9badbb53dfff19bbe8b4cc27fd1a81c655dc0f
Josue-lucas <josuelucasfullstack@gmail.com>
2023-07-02 00:48:42 +0000

    Geração de um gráfico de linha utilizando os dados do arquivo gasolina.csv com o dia no eixo x e o seu preço no eixo y, em seguida salvamos o gráfico no arquivo gasolina.png, foi criado um arquivo gasolina.py com os comandos para gerar o gráfico

---
+++ gasolina.py
@@ -0,0 +1,22 @@
+# código de geração do gráfico 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Carregar os dados do arquivo CSV
+df = pd.read_csv('gasolina.csv')
+
+# Criar o gráfico de linha
+plt.plot(df['dia'], df['venda'])
+
+# Configurar os rótulos dos eixos
+plt.xlabel('Dia')
+plt.ylabel('Preço')
+
+# Configurar o título do gráfico
+plt.title('Preço da Gasolina ao Longo dos Dias')
+
+# Salvar o gráfico no arquivo PNG
+plt.savefig('gasolina.png')
+
+# Exibir o gráfico
+plt.show()
